f1ad68daca8fd6c2573c1438c4c23efd04b034c2
jumpyguest <roland.almendras.salazar@gmail.com>
2025-01-17 17:41:32 +0800

    Implement calculator GUI and operation functions
    Initial implementation of the calculator user interface.

---
+++ javascript.js
@@ -0,0 +1,37 @@
+let num1, num2 = 0;
+let operator = "";
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, op) {
+    switch(op) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subtract(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return divide(a, b);
+            break;
+        default:
+            break;
+    }
+}
