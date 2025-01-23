4c99e48e70b647f28c6b1034e27f80c50369cfe8
jumpyguest <roland.almendras.salazar@gmail.com>
2025-01-24 00:29:41 +0800

    Implement +/- button functionality

--- javascript.js
+++ javascript.js
@@ -2,14 +2,17 @@ let num1 = "", num2 = "";
 let operator = "";
 let storeInNum1 = true;
 let clearDisplay = true;
+let signedNum1 = false;
+let signedNum2 = false;
 const numBtns = document.querySelectorAll(".digit");
 const opBtns = document.querySelectorAll(".op");
 const equalsBtn = document.querySelector("#equalsBtn");
 const acBtn = document.querySelector("#AC");
 const percentBtn = document.querySelector("#percent");
-const screen = document.querySelector("#screen1");
 const delBtn = document.querySelector("#delete");
 const pointBtn = document.querySelector("#point");
+const signBtn = document.querySelector("#sign");
+const screen = document.querySelector("#screen1");
 
 function add(a, b) {
     return +a + +b;
@@ -80,22 +83,26 @@ pointBtn.addEventListener("click", () => {
     pointHandler();
 });
 
+signBtn.addEventListener("click", () => {
+    signHandler();
+});
+
 /** Event handlers */
 
 function digitHandler(e) {
     storeOperands(e.target.textContent);
-    displayToScreen(e.target.textContent);
+    displayToScreen();
 }
 
 function opHandler(e) {
     processOp(e.target.textContent);
-    displayToScreen(num1 + e.target.textContent);
+    displayToScreen();
 }
 
 function equalsHandler() {
     if (num1 !== '' && num2 !== '') {
         getAnswer();
-        displayToScreen(num1);
+        displayToScreen();
     }
 }
 
@@ -103,32 +110,32 @@ function percentHandler() {
     clearDisplay = true;
     if (storeInNum1 || operator === "") {
         num1 /= 100;
-        displayToScreen(num1);
-    } else if (operator !== "") {
+        displayToScreen();
+    } else if (operator !== "" && num2 !== "") {
         num2 /= 100;
-        displayToScreen(num1 + operator + num2)
+        displayToScreen()
     }
 }
 
 function deleteHandler() {
     clearDisplay = true;
     if (storeInNum1) {
-        if (num1.toString().length === 1) {
+        if (num1.toString().length == 1) {
             num1 = "";
-            displayToScreen(0);
+            displayToScreen();
             clearDisplay = true;
         } else {
             num1 = num1.toString().slice(0, -1);
-            displayToScreen(num1);
+            displayToScreen();
         }
         console.log(`num1:${num1}`);
     } else if (num2 === "" && operator !== "") {
-        displayToScreen(num1);
         operator = "";
+        displayToScreen();
         storeInNum1 = true;
     } else {
         num2 = num2.toString().slice(0, -1);
-        displayToScreen(num1 + operator + num2)
+        displayToScreen()
         console.log(`num2:${num2}`);
     }
 }
@@ -136,19 +143,59 @@ function deleteHandler() {
 function pointHandler() {
     if ((storeInNum1 && num1.indexOf(".") === -1) || (!storeInNum1 && num2.indexOf(".") === -1)) {
         storeOperands(".");
-        displayToScreen(".");
+        displayToScreen();
     } 
+}
+
+function signHandler() {
+    if (operator === "" && storeInNum1) {
+        if (!signedNum1) {
+            signedNum1 = true;
+            num1 = `-${num1}`;
+            clearDisplay = true;
+            displayToScreen();
+        } else {
+            signedNum1 = false;
+            num1 = num1 * (-1);
+            clearDisplay = true;
+            displayToScreen();
+        }
+    } else if (num2 !== "") { 
+        if (!signedNum2) {
+                signedNum2 = true;
+                num2 = `-${num2}`;
+                clearDisplay = true;
+                displayToScreen();
+        } else {
+                signedNum2 = false;
+                num2 = num2 * (-1);
+                clearDisplay = true;
+                displayToScreen();
+        }
+    }
 }
 
 /** Utility functions */
 
-function displayToScreen(number){
+function displayToScreen() {
+    let displayNum1 = num1;
+    let displayNum2 = num2;
+    if (signedNum1) {
+        displayNum1 = `(${num1})`;
+    }
+    if (signedNum2) {
+        displayNum2 = `(${num2})`;
+    }
+
     if (clearDisplay) {
         screen.textContent = "";
         clearDisplay = false;
     }
-    
-    screen.textContent += `${number}`;
+    if (num1 === "") {
+        screen.textContent = 0;
+    } else {
+        screen.textContent = displayNum1 + operator + displayNum2;
+    }
 }
 
 function processOp(op) {
@@ -163,6 +210,7 @@ function processOp(op) {
 }
 
 function storeOperands(digit) {
+    /** This if-condition is met when user presses a digit after pressing equals button */
     if (operator === "" && storeInNum1 === false) {
         clearDisplay = true;
         storeInNum1 = true;
@@ -179,10 +227,18 @@ function storeOperands(digit) {
 }
 
 function getAnswer() {
+    if (num1 === ".") {
+        num1 = 0;
+    }
+    if (num2 === ".") {
+        num2 = 0;
+    }
     num1 = +parseFloat(operate(num1, num2, operator).toFixed(11)).toString();
     num2 = "";
     operator = "";
     clearDisplay = true;
+    signedNum1 = false;
+    signedNum2 = false;
 }
 
 function clearAll() {
@@ -192,4 +248,6 @@ function clearAll() {
     operator = "";
     screen.textContent = "0";
     clearDisplay = true;
+    signedNum1 = false;
+    signedNum2 = false;
 }
